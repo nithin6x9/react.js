56b789c5d4dd8689d1de060c5c36c96908442381
nithin-nk <nithinkrishnalal303@gmail.com>
2024-01-08 01:33:40 +0530

    update

--- To-Do/src/App.jsx
+++ To-Do/src/App.jsx
@@ -1,23 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import React from 'react'
-
+import React,{ useState} from 'react'
+/*
 function App() {
   const [inputValue, setinputValue] = useState('')
-  function onclickHandler(){
-
+  const handleInputValue = (event) =>{
+    setinputValue(event.target.value)
 
   }
+  
+}
+function onclickHandler(){
+    
+    
 
   return (
     <div>
-    <input id="todo" type = "text" placeholder="Add your todo here!" value={inputValue}></input><br></br>
+    <input id="todo" type = "text" placeholder="Add your todo here!" value={inputValue} onchange={handleInputValue}></input>
+    <br></br>
     <br></br>
     <button onClick={onclickHandler}>Add to your List!</button>
     </div>
   )
+}
+
+export default App*/
+function App(){
+    const[count,setCount]=React.useState(0)
+    
+    return(
+        <div>
+            <button count ={count} setCount={setCount}></button>
+
+        </div>
+    )
+}
+function Button(props){
+    function onButtonClick(){
+        props.setCount(props.count+1);
+    }
+    return <button onClick={onButtonClick}>Counter{props.count}</button>
 }
 
 export default App
