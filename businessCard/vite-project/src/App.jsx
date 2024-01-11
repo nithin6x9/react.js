a498e004cb16b111140201c231ec4b9bc938ff72
nithin-nk <nithinkrishnalal303@gmail.com>
2024-01-11 19:28:40 +0530

    removed css files

--- businessCard/vite-project/src/App.jsx
+++ businessCard/vite-project/src/App.jsx
@@ -1,6 +1,16 @@
 export function BusinessCard(props){
     return(
-        <div>
+        <div style="{styles.card}">
+            <h2 style="{styles.name}"></h2>
+            <p style="{styles.description}"></p>
+            <h3 style="{styles.intrestsHeader">Intrests</h3>
+            <ul styles="{styles.intresList">
+                {props.intrests.map((intrest)=>{
+                    <li key = {intrest} style = {style.intrestItems}>
+                        {intrest}
+                    </li>
+                })}
+            </ul>
             
         </div>
     )
