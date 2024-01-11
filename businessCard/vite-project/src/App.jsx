39c6ac885855dde14f753ed2816e6e16470d7795
nithin-nk <nithinkrishnalal303@gmail.com>
2024-01-12 00:53:04 +0530

    all the css elements added

--- businessCard/vite-project/src/App.jsx
+++ businessCard/vite-project/src/App.jsx
@@ -44,6 +44,36 @@ const styles = {
         fontSize:'10px',
         color:'#555',
         marginBottom:'5px',
-    }
+    },
+    sociallinks:{
+        display:'flex',
+        marginBottom:'15px',
+    },
+    link:{
+        textDecoration:'none',
+        color:'#fff',
+        padding:'10px 15px',
+        borderRadius:'5px',
+        backgroundColor:'black',
+        display:'inline-block',
+        margin:'10px',
+        marginShadow:'0 2px 4px rgba(0,0,0,0.1)',
     
+    },
+    intrestsHeader:{
+        fontSize:'18px',
+        marginBottom:'10px',
+        color:'#033',
+    },
+    intrestList:{
+        listStyle:'none',
+        padding:'0',
+        margin:'0',
+    },
+    intrestItem:{
+        fontSize:'14px',
+        marginBottom:'5px',
+        color:'#555'
+    }
+
 }
