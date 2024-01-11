cba22be5731490f85b26c14fdc3c73cb51041ad0
nithin-nk <nithinkrishnalal303@gmail.com>
2024-01-11 21:34:50 +0530

    react part completed

--- businessCard/vite-project/src/App.jsx
+++ businessCard/vite-project/src/App.jsx
@@ -11,7 +11,16 @@ export function BusinessCard(props){
                     </li>
                 })}
             </ul>
-            
+            <div style = "{styles.socialLinks}">
+                <a href = {props.linkedin} target = "_blank" rel = "noopener noreferrer" style={{...styles.link,marginLeft:'0px'}}>linkedin</a>
+                <br />
+                <a href = {props.twitter} target = "_blank" rel= "noopener noreferrer" styles = {styles.link}>Twitter</a>
+            {props.othersocialmedia &&(
+            <a href={props.othersocialmedia} target ="_blank" rel="noopener noreferrer" style = {styles.link}>
+                {props.othersocialmedia.label}
+            </a>
+            )}
+            </div>
         </div>
     )
 }
