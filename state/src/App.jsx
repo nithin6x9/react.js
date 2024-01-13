dc17030b4784932acf66807f7c2f025ddbd377a7
nithin-nk <nithinkrishnalal303@gmail.com>
2024-01-13 23:21:49 +0530

    update

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,16 +1,37 @@
 import { useState } from 'react'
-import './App.css'
-function App() {
-    const [count,setCount] = useState(0);
+import './App.css';
+
+function NameList(){
+    const [List,setList] = useState(["Jack","Nithin","Jhon"]);
+    return(
+        <div>
+            <ul>
+                {List.map((name)=>{
+                    <li key = {name}>{name}</li>
+                })}
+            </ul>
+        </div>
+    );i
+}
+
+function Counter() {
+    let [count,setCount] = useState(0);
 
     function addOne(){
         setCount(count+1);
     }
   return (
-    <div className="App">
+    <div>
         <button onClick={addOne}>count={count}</button>
     </div>
   )
 }
-
+function App(){
+    return(
+        <div>
+            <Counter />
+            <NameList />
+        </div>
+    )
+}
 export default App
