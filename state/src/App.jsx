34a073a23b27329f1004f04ca6de4f145b633ecc
nithin-nk <nithinkrishnalal303@gmail.com>
2024-01-13 20:57:00 +0530

    css

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-
+import './App.css'
 function App() {
     const [count,setCount] = useState(0);
 
     function addOne(){
-        count++;
+        setCount(count+1);
     }
   return (
     <div className="App">
