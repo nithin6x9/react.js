4e46ab153e6614f5a272faa4588a1169536eb823
nithin-nk <nithinkrishnalal303@gmail.com>
2024-01-14 13:53:32 +0530

    useState pakka

---
+++ state/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+function App(){ 
+    const [count,setCount] = useState(0);
+
+    function increment(){
+        setCount(count+1);
+    }
+
+    return(
+        <div>
+            <h1>Incrementer/Decrementer{count}</h1>
+            <button onClick = { ()=>{
+                setCount(count-1);
+            }}>Decrement</button>
+            <button onClick = { ()=>{
+                setCount(0);
+            }}>Clear</button>
+            <button onClick = {increment}>increment</button>
+        </div>
+    )
+}
+
+export default App
