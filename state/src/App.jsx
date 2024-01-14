89f558aaf1a0a2027858c7d40b17640fff2ab00c
nithin-nk <nithinkrishnalal303@gmail.com>
2024-01-14 14:09:56 +0530

    useEffect

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,7 +1,12 @@
-import { useState } from 'react'
+import {useEffect, useState } from 'react'
 
 function App(){ 
     const [count,setCount] = useState(0);
+
+    useEffect(()=>{
+        console.log("Updated count: ",count);
+    },[count]);
+
 
     function increment(){
         setCount(count+1);
