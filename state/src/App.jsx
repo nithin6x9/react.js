205de4f7edb6b2f53a3fc6f0a87e3919b9dae549
nithin-nk <nithinkrishnalal303@gmail.com>
2024-02-02 17:48:21 +0530

    app

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -2,5 +2,23 @@ import React,{Fragment,useState} from 'react';
 
 function App(){
     const [title,setTitle] = useState("Hola");
-    
+    return(
+        <div>
+
+        </div>
+    )
 }
+
+function Todo({title,description}){
+    return (
+        <div>
+        <h1>
+            {title}
+        </h1>
+        <h5>
+            {description}
+        </h5>
+        </div>
+    )
+}
+export default App
