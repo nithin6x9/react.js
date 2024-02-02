ed241bed4c29994b3b7a02e7577e8f0235404f6f
nithin-nk <nithinkrishnalal303@gmail.com>
2024-02-02 17:46:15 +0530

    change

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,49 +1,6 @@
-import { useState,memo } from 'react'
-import './App.css'
-/*
-function App() {
-  const [count, setCount] = useState(0)
-    function add() {
-        setCount(count+1);
-    }
-  return (
-   <div className="counter">
-    <h1>Count:{count}</h1>
-    <button onClick={add}>Increment</button>
-   </div>
-  )
-}
-
-export default <App>*/
+import React,{Fragment,useState} from 'react';
 
 function App(){
-    return(
-        <div>
-            <Headerwithbutton />
-            <h2>Lol! i made it kudos</h2>
-            <h3>hehehehe</h3>
- 
-        </div>
-    )}
-    function Headerwithbutton(){
-    const [firsttitle, setfirsttitle] = useState("My name is Nithin krishna");
-    function changetitle(){
-        setfirsttitle("My name is "+Math.random());
-        //setName("oomb eni ella xD!")
-    }
-    return<>
-        <button onClick={changetitle}>Click to change Name</button>
-        <Header title={firsttitle} />
-    </>
-    }
-    function Header({title}) {
-        return <div>
-           <br></br> {title}
-        </div>
-    }
-    const Header = React.memo(function Header({title}){
-        return <div>
-            {title}
-        </div>
-    })
-export default App
+    const [title,setTitle] = useState("Hola");
+    
+}
