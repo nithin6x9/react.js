e13cc34430fc5720e4853e1172fec798baaf3736
nithin-nk <nithinkrishnalal303@gmail.com>
2024-02-12 14:24:55 +0530

    update

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,1 +1,28 @@
-import React from 'react-dom'
+import {useState,useEffect} from 'react'
+import axios from "axios";
+
+function App(){
+    useEffect(() => {
+        axios.get("https://sum-server.100xdevs.com/todos")
+        .then(function(response){
+            setTodos(response.data.todos)
+    })
+},[]);
+
+    return(
+        <>
+            {todos.map(todo => <Todo title={todo.title} description = {todo.description} />)}
+        </>
+    )
+
+}
+
+function Todo({title,description}){
+    return <div>
+        <h1>{title}
+        </h1>
+        {description}
+    </div>
+}
+
+export default App()
