f3f3cb893dca1038629a54efdec68360485c98f4
nithin-nk <nithinkrishnalal303@gmail.com>
2024-02-21 20:48:05 +0530

    useCallback

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,26 +1,26 @@
-import {useState,useEffect,useMemo} from 'react'
-
-
+import {useState,memo} from 'react'
 
 function App(){
-    const [inputValue,setInputValue]  = useState(0);
-    const [counter,setCounter] = useState(0);
+        const [count,setCount] = useState(0);
 
-    let Count = useMemo(()=>{
-        console.log("Memo got called")
-        let finalCount = 0;
-            for(let i = 1;i<=inputValue;i++){
-                finalCount += i;
-            }
-            return finalCount;
-    },[inputValue])
-    return     <div>
-    <input placeholder = {"Enter the number"} type = "text" value = {inputValue} onChange ={(e) => setInputValue(e.target.value)} /><br /><br />
-    <button onClick={() => {setCounter(counter+1);}}>
-      Click to sum the value from 1 to {inputValue} is {Count}
-    </button>
-    <p>Sum from 1 to {inputValue} is {Count}</p>
-    </div>
+        function onClick(){
+            console.log("Clicked");
+        }
+
+        return <div>
+            <Child onClick={onClick} />
+            <button onClick={() => {
+                setCount(count+1);
+            }}>Click me</button>
+        </div>
 }
+
+const Child = memo(({onClick}) => {
+    console.log("Child render")
+
+    return <div>
+        <button onClick = {onClick}>Button Clicked</button>
+    </div>
+})
 
 export default App;
