2a9c449dc650bdded1fc1a4a84e5b393133aef2d
nithin-nk <nithinkrishnalal303@gmail.com>
2024-02-21 19:32:07 +0530

    Adder using useMemo

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,31 +1,26 @@
-import {useState,useEffect} from 'react'
-import axios from 'axios'
+import {useState,useEffect,useMemo} from 'react'
+
 
 
 function App(){
     const [inputValue,setInputValue]  = useState(0);
-    const [sum,setSum] = useState(0);
-    const [finalValue,setFinalValue] = useState(0);
+    const [counter,setCounter] = useState(0);
 
-    useEffect(()=>{
-        if(inputValue>0){
-            let sum = 0;
+    let Count = useMemo(()=>{
+        console.log("Memo got called")
+        let finalCount = 0;
             for(let i = 1;i<=inputValue;i++){
-                sum+=i;
+                finalCount += i;
             }
-            setSum(sum)
-        } 
-        setFinalValue(sum);
+            return finalCount;
     },[inputValue])
-    return (
-    <div>
+    return     <div>
     <input placeholder = {"Enter the number"} type = "text" value = {inputValue} onChange ={(e) => setInputValue(e.target.value)} /><br /><br />
-    <button onClick={() => setInputValue(parseInt(inputValue))}>
-      Click to sum the value from 1 to {inputValue}
+    <button onClick={() => {setCounter(counter+1);}}>
+      Click to sum the value from 1 to {inputValue} is {Count}
     </button>
-    <p>Sum from 1 to {inputValue} is {finalValue}</p>
+    <p>Sum from 1 to {inputValue} is {Count}</p>
     </div>
-    )
 }
 
 export default App;
