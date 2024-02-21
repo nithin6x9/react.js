85c44ea49e10a4c1971fb8217314f41f7ec5166c
nithin-nk <nithinkrishnalal303@gmail.com>
2024-02-21 13:59:06 +0530

    ugly and not working

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,29 +1,38 @@
 import {useState,useEffect} from 'react'
+import axios from 'axios'
 
 
 function App(){
-    const [todos,setTodos] = useState([]);
-
-    useEffect(()=>{
-        axios.get("https://sum-server.100xdevs.com/todo?id=1")
-        .then(function(response){
-            setTodos(response.data.todos)
-        })
-    },[]);
-
-
-    return(
+    return (
         <>
-            {todos.map(todo => <Todo title = {todo.title} description={todo.description} />)}
+          <button onClick={() => {<Todo id={1} />}}>1</button>
+          <button onClick={() => {<Todo id={2} />}}>2</button>
+          <button onClick={() => {<Todo id={3} />}}>3</button>
+          <button onClick={() => {<Todo id={4} />}}>4</button>
         </>
-    )
+      );
+}
+function one(){
+
 }
 
-function Todo({title,description}){
+function Todo({id}){
+    const[todo,setTodo] = useState({});
+
+    useEffect(()=>{
+        axios.get("https://sum-server.100xdevs.com/todo?id="+id)
+        .then(response =>{
+            setTodo(response.data.todo)
+        })
+    })
     return <div>
-        <h1>{title}</h1>
-        {description}
+    <h1>
+        {todo.title}
+    </h1>
+    <h4>
+        {todo.description}
+    </h4>
     </div>
 }
 
-export default App
+export default App;
