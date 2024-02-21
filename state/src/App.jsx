f1cea40fcca07c0f2b6e944f081d2df8b25e3606
nithin-nk <nithinkrishnalal303@gmail.com>
2024-02-21 14:20:37 +0530

    Button todo

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -3,16 +3,24 @@ import axios from 'axios'
 
 
 function App(){
-    return (
-        <>
-          <button onClick={() => {<Todo id={1} />}}>1</button>
-          <button onClick={() => {<Todo id={2} />}}>2</button>
-          <button onClick={() => {<Todo id={3} />}}>3</button>
-          <button onClick={() => {<Todo id={4} />}}>4</button>
-        </>
-      );
-}
-function one(){
+    const [selectedId,setSelectedId] = useState(1);
+
+    return  <div>
+          <button onClick = {function(){
+            setSelectedId(1);
+          }}>1</button>
+          <button onClick = {function(){
+            setSelectedId(2);
+          }}>2</button>
+          <button onClick = {function(){
+            setSelectedId(3);
+          }}>3</button>
+          <button onClick = {function(){
+            setSelectedId(4);
+          }}>4</button>
+
+            <Todo id = {selectedId} />
+        </div>
 
 }
 
@@ -20,12 +28,13 @@ function Todo({id}){
     const[todo,setTodo] = useState({});
 
     useEffect(()=>{
-        axios.get("https://sum-server.100xdevs.com/todo?id="+id)
+        axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`)
         .then(response =>{
             setTodo(response.data.todo)
         })
-    })
+    },[id])
     return <div>
+    Id:{id}
     <h1>
         {todo.title}
     </h1>
