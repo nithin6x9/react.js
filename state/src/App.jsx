8a625c5ae1e49137da4bd06c2ba7498f2e7a15b3
nithin-nk <nithinkrishnalal303@gmail.com>
2024-02-23 21:25:52 +0530

    context api

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,12 +1,18 @@
 
 import {useState} from 'react';
+import {CountContext} from './context';
+
+//Context API
 function App(){
 
     const [count,setCount] = useState(0);
     return (
     <div>
+    <CountContext.Provider value = {count}>
+
         <Count  count = {count}/>
         <Buttons count = {count} setCount={setCount}/>
+    </CountContext.Provider>
     </div>
     )
 }
@@ -16,6 +22,13 @@ function Count({count}){
         {count}
     </div>
 
+}
+
+function CountRenderer(){
+    const count = useContext(CountContext);
+    return <div>
+        {count}
+    </div>
 }
 
 function Buttons({count,setCount}){
