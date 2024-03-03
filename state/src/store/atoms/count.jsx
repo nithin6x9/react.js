62939cf43e12ed967f465ee479b9b756430cae45
nithin-nk <nithinkrishnalal303@gmail.com>
2024-03-03 16:39:37 +0530

    selector

--- state/src/store/atoms/count.jsx
+++ state/src/store/atoms/count.jsx
@@ -1,6 +1,15 @@
-import { atom } from 'recoil';
+import {createContext} from "react";
+import { selector,atom } from 'recoil';
 
 export const countAtom = atom({
     key: "countAtom",
     default:  0
 });
+
+export const evenSelctor = selector({
+    key: "evenSelctor",
+    get: ({get})=>{
+        const count = get(countAtom);
+        return count %2;
+    }
+})
