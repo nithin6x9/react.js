83f08cf6b53f34671ffb2eaff1727000120fe1b0
nithin-nk <nithinkrishnalal303@gmail.com>
2024-03-03 12:50:59 +0530

    recoil

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,3 +1,24 @@
 import {useContext,useState} from 'react'
 import {CountContext} from "./context"
 
+function App(){
+    const [count,setCount] = useState();
+
+
+    return (
+        <>
+        <CountContext.Provider value = {count}>
+            <count setCount = {setCount} />
+        </CountContext.Provider>
+        </>
+    )
+}
+
+function Count({setCount}){
+    
+    return <div>
+        <CountRenderer />
+        <Buttons  setCount = {setCount} />
+    </div>
+
+}
