a171b7c4e4e4f103edb8882f783fd9891a5a4261
nithin-nk <nithinkrishnalal303@gmail.com>
2024-03-05 16:30:49 +0530

    profile-component

--- profile_component/src/App.jsx
+++ profile_component/src/App.jsx
@@ -1,5 +1,7 @@
 //WEEK-7 ASSIGNMENT 1
 import Card from './Component.jsx'
+
+
 function App() {
     return <div>
         <Card />
--- profile_component/src/Component.jsx
+++ profile_component/src/Component.jsx
@@ -1,10 +1,16 @@
-import myimage from './assets/profile1.png'
+import React from 'react';
+import myimage from './assets/download.png'
+import './index.css'
 
 function Card(){
     return(
         <div className="card">
-            <img src={myimage} />
-            <h2>Rita Correia 32</h2>
+            <img className ="card-image" src={myimage} />
+               <div className="card-subdiv">
+                <strong>Apple  </strong>32
+                </div>
+            <p className="card-para">Designed in California,Assembled in INDIA</p>
+
         </div>
     )
 
