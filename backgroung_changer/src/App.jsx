223eea1fe957d33e95397203827e5735423ddef1
nithin-nk <nithinkrishnalal303@gmail.com>
2024-03-05 19:10:13 +0530

    update

--- backgroung_changer/src/App.jsx
+++ backgroung_changer/src/App.jsx
@@ -1,7 +1,20 @@
 import './App.css'
 
+
+
 function App() {
-        
+    return(
+        <div clasName="button">
+            <button> green</button>
+            <button> green</button>
+            <button> green</button>
+            <button> green</button>
+            <button> green</button>
+            <button> green</button>
+            <button> green</button>
+        </div>
+    )
+
 }
 
 export default App
