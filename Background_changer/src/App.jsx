bb4e2e13cca4d9b7ad931808e46929d75264bcc0
nithin-nk <nithinkrishnalal303@gmail.com>
2024-03-06 00:23:57 +0530

    positioned color picker to bottom

--- Background_changer/src/App.jsx
+++ Background_changer/src/App.jsx
@@ -5,21 +5,36 @@ import './App.css'
 
 function App() {
     return(
-        <RecoilRoot>
-            
-        </RecoilRoot>
+        <>
+            <Change />
+        </>
     )
 
 }
-function change(){
-    return <div>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
+function Change(){
+    const [colour,setColour] = useState("white")
+    return <div className="Change-butt">
+        <button className="Red-button" onClick={()=>{
+            setCount("red")
+        }}>RED</button>
+        <button className="Yellow-button" onClick={()=>{
+            setCount("red")
+        }}>YELLOW</button>
+        <button className="Black-button" onClick={()=>{
+            setCount("red")
+        }}>BLACK</button>
+        <button className="Purple-button" onClick={()=>{
+            setCount("red")
+        }}>PURPLE</button>
+        <button className="Green-button" onClick={()=>{
+            setCount("red")
+        }}>GREEN</button>
+        <button className="Blue-button" onClick={()=>{
+            setCount("red")
+        }}>BLUE</button>
+        <button className="Default-button" onClick={()=>{
+            setCount("red")
+        }}>Default</button>
 
     </div>
 }
