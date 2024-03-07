868512230b53a796078814d6c5b8e8e3905e97cc
nithin-nk <nithinkrishnalal303@gmail.com>
2024-03-08 01:23:18 +0530

    frontend complete

--- birthday-card/src/App.jsx
+++ birthday-card/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import {useState} from 'react';
 
 function App() {
   return <div>
@@ -6,12 +7,22 @@ function App() {
   </div>
 }
 function BirthdayCard(){
+    const [name,setName] = useState()
+    const [value,storeValue] = useState()
+    const handleInputChange = (event) => {
+        setName(event.target.value);
+    };
+    const handleButtonClick = ()=>{
+        storeValue(name);
+    };
+
     return(
     <div className = "TopDiv">
         <p className="Name">Enter Your Name!</p>
         <div className="subDiv">
-        <input className="Input-box" type = "text" placeholder="Enter your Name!"></input><br />
-        <button className="Button">Done</button>
+        <input className="Input-box" type = "text" placeholder="Enter your Name!" value={name} onChange={handleInputChange}></input><br />
+        <button className="Button" onClick={handleButtonClick}>Done</button>
+        
         </div>
     </div>
     )
