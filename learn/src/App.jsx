a0c04ee9457424ebcbcc9aaf7a2014ec464ed98a
nithin-nk <nithinkrishnalal303@gmail.com>
2024-03-10 16:33:01 +0530

    Tailwind

---
+++ learn/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+export default function App(){
+    return <div className='flex'>
+        <div>
+            <button>HEllow i am nithin</button>
+        </div>
+        <div>
+            <button>HEllow i am nithin</button>
+        </div>
+        <div>
+            <button>HEllow i am nithin</button>
+        </div>
+        <div>
+            <button>HEllow i am nithin</button>
+        </div>
+        <div>
+            <button>HEllow i am nithin</button>
+        </div>
+        <div>
+            <button>HEllow i am nithin</button>
+        </div>
+        <div>
+            <button>HEllow i am nithin</button>
+        </div>
+    </div>
+}
