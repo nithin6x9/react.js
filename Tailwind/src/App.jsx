2156cf5fbacef702d63a24b8c89e1806a4d3b831
nithin-nk <nithinkrishnalal303@gmail.com>
2024-03-10 18:39:39 +0530

    Tailwind

---
+++ Tailwind/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+export default function App(){
+    return(
+        <>
+            <div style={{display:'flex',justifyContent:'center'}}>
+                <div>Button</div>
+                <div>Button</div>
+                <div>Button</div>
+                <div>Button</div>
+                <div>Button</div>
+                <div>Button</div>
+            </div>
+        </>
+    )
+}
