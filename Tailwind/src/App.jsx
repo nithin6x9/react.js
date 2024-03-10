4bdc825e0c540e4b75414ba1930497b60d617dc7
nithin-nk <nithinkrishnalal303@gmail.com>
2024-03-10 23:12:51 +0530

    Tailwind

--- Tailwind/src/App.jsx
+++ Tailwind/src/App.jsx
@@ -1,15 +1,19 @@
 
 export default function App(){
     return(
-      <div className='grid grid-cols-6'>
-           <div className='bg-red-500'>Nithin</div>
-           <div>Nithin</div>
-           <div>Nithin</div>
-           <div>Nithin</div>
-           <div>Nithin</div>
-           <div>Nithin</div>
-
-
-      </div>
-    )
-  }
+        <>    
+    <div className='grid grid-cols-1 sm:grid-cols-4 lg:grid-cols-3'>
+      <div className='flex justify-center bg-red-500 rounded-full'>Nithin</div>
+      <div className='flex justify-center bg-yellow-500 rounded-full'>Nithin</div>
+      <div className='flex justify-center bg-pink-500 rounded-full'>Nithin</div>
+      <div className='flex justify-center bg-red-500 rounded-full'>Nithin</div>
+      <div className='flex justify-center bg-yellow-500 rounded-full'>Nithin</div>
+      <div className='flex justify-center bg-pink-500 rounded-full'>Nithin</div>
+      <div className='flex justify-center bg-red-500 rounded-full'>Nithin</div>
+      <div className='flex justify-center bg-yellow-500 rounded-full'>Nithin</div>
+      <div className='flex justify-center bg-pink-500 rounded-full'>Nithin</div>
+      
+    </div>
+        </>
+    
+    )  }
