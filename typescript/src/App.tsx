9b5c3948edd1998f3c4d37aa9c4ba6d898b86afd
nithin-nk <nithinkrishnalal303@gmail.com>
2024-03-31 14:43:08 +0530

    hooks

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,28 +1,41 @@
-import { useState } from 'react'
-
+import React, { useState, useEffect } from 'react';
+// Remove the following line if you're not using it
+// import { escapeComponent } from '../node_modules/uri-js/dist/esnext/uri';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    <MyComponent />
-    </>
-    
-  )
-}
-function MyComponent(){
-    const [count,setCount] = useState(0);
-
-    const incrementCount = ()=>{
-        setCount(count+1);
-    };
+  const [render, setRender] = useState(true);
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setRender(false);
+    }, 1000); // Comma was missing here
+  }, []);
+  
+  if (!render) {
     return (
-        <div>
-            <p>{count}</p>
-            <button onClick={incrementCount}>Increment</button>
-        </div>
-    )
+      <div>
+        {render ? <MyComponent /> : <div></div>}
+      </div>
+    );
+  }
+
+  function MyComponent() {
+    useEffect(() => {
+      console.error("Component mounted");
+      return () => {
+        console.log("Component Unmounted");
+      };
+    }, []);
+
+    return (
+      <div>
+        from inside my escapeComponent
+      </div>
+    );
+  }
+
+  // Make sure you return something if render is true
+  return <div>Loading...</div>;
 }
 
-export default App
+export default App;
