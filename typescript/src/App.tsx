d0e52a5b6a57baa0ccf2ea73f532126dff238d53
nithin-nk <nithinkrishnalal303@gmail.com>
2024-04-02 23:54:42 +0530

    update

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react';
 //useDimenesion hook to find the height and width of the browser 
-function useInterval(){
-    const [count, setCount] = useState(0);
-    
-    useInterval(() => {
-      setCount(c => c + 1);
-    }, 1000)
-
+function useInterval(fn,timeOut){
+    useEffect(()=>{
+            setInterval(fn,timeout);
+    },[])
 }
 
 function App() {
+    const [count,setCount] = useState(0);
+
+    useInterval(()=>{
+        setCount(c => c+1);
+    },1000)
   
     return (
       <>
