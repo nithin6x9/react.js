074f56b64309adb2c0442d021a109e61fa7675e7
nithin-nk <nithinkrishnalal303@gmail.com>
2024-04-02 09:55:58 +0530

    auto refresh

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,20 +1,31 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-function useTodos() {
+function useTodos(n) {
     const [todos, setTodos] = useState([])
-    
+    const [loading,setLoading]  = useState(true);
     useEffect(() => {
-      axios.get("https://sum-server.100xdevs.com/todos")
+        setInterval(()=>{
+        axios.get("https://sum-server.100xdevs.com/todos")
+            .then(res => {
+                setTodos(res.data.todos);
+                setLoading(false)
+        })
+        },n*1000)
+        axios.get("https://sum-server.100xdevs.com/todos")
         .then(res => {
-          setTodos(res.data.todos);
+            setTodos(res.data.todos);
+            setLoading(false)
         })
     }, [])
-    return todos;
+    return {todos, loading};
 }
 
 function App() {
-    const todos = useTodos();
+    const {todos,loading} = useTodos(5);
+    if (loading){
+        return <div>Loading.....</div>
+    }
   return (
     <>
       {todos.map(todo => <Track todo={todo} />)}
