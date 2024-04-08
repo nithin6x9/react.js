65f252302dee534567dab5b795bf0158186f407d
nithin-nk <nithinkrishnalal303@gmail.com>
2024-04-08 22:46:50 +0530

    rootDir outDir

---
+++ learn.ts/dist/learn.js
@@ -0,0 +1,13 @@
+"use strict";
+/*function islegal(age:number){
+    if (age >= 18) {
+        return true
+    }else{
+        return false
+    }
+}
+
+islegal(19)*/
+function runAfter15(fn) {
+    setTimeout(fn, 10000);
+}
