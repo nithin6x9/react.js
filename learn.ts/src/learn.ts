8bab7a663b0d66394f732f1517d2c771c0c93c77
nithin-nk <nithinkrishnalal303@gmail.com>
2024-04-10 18:14:35 +0530

    enum

--- learn.ts/src/learn.ts
+++ learn.ts/src/learn.ts
@@ -6,8 +6,39 @@
     }
 }
 
-islegal(19)*/
+islegal(19)
 
 function runAfter15(fn: ()=> void){
     setTimeout(fn,10000)
 }
+
+interface User{
+    firstName:string,
+    lastName:string,
+    age: number,
+}
+function isLegal(user:User){
+    if(user.age >18){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+isLegal({
+    firstName:"Nithin",
+    lastName:"Krishna",
+    age:21,
+})
+*/
+
+enum Direction{
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+
+doSomethin(Direction.Right);
+function DoSomething()
